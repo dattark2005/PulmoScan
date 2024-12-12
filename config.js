8c0a3b6d77828d5e48cac0ccdfd67461a7d372e8
Dattatray Kshirsagar <dattatraykshirsagar2005@gmail.com>
2024-12-12 20:37:22 +0530

    Application completed

--- config.js
+++ config.js
@@ -3,13 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA_9lz4DzfOkLWP3ufLyQMkUxzYLmSuLAs",
-    authDomain: "medical-app-2a024.firebaseapp.com",
-    projectId: "medical-app-2a024",
-    storageBucket: "medical-app-2a024.appspot.com",
-    messagingSenderId: "809229460718",
-    appId: "1:809229460718:web:09740414bbb8c28bcaffff",
-    measurementId: "G-CM7SDTZ9V1"
+    apiKey: "AIzaSyBX3v707q9UCXARla6-hFm1tYZAvd6KqKw",
+    authDomain: "pulmoscan-f3404.firebaseapp.com",
+    databaseURL: "https://pulmoscan-f3404-default-rtdb.firebaseio.com",
+    projectId: "pulmoscan-f3404",
+    storageBucket: "pulmoscan-f3404.firebasestorage.app",
+    messagingSenderId: "419252092339",
+    appId: "1:419252092339:web:44b23e74eca05099429fc6",
+    measurementId: "G-9EN7WRPKYV"
 };
 
 if (!firebase.apps.length) {
